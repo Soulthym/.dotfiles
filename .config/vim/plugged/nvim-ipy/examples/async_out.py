b25cbf3b9cc469c4303147f04638064bbe5f5440
Soulthym <thymi92@gmail.com>
2020-02-09 19:01:29 +0100

    compat

--- .config/vim/plugged/nvim-ipy/examples/async_out.py
+++
@@ -1,7 +0,0 @@
-from time import sleep
-import sys
-sleep(1)
-for i in range(10):
-    sleep(0.3)
-    print(i)
-    sys.stdout.flush()
--- .config/vim/plugged/vim-open-color/ci/light_256.vim
+++
@@ -1,6 +0,0 @@
-set background=light
-colorscheme open-color
-
-TOhtml
-w! dist/light_256.html
-qall!
