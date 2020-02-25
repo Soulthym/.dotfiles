f54e2fdb8959883b58d8a3796463dd0ee5f68cd4
Soulthym <thymi92@gmail.com>
2020-02-25 14:48:57 +0100

    minor fixes+sound fix+ssd

--- .scripts/bar.sh
+++ .scripts/bar.sh
@@ -16,9 +16,14 @@ echo '{ "version": 1 } [ []'
 while :; do
     printf ',['
     module\
-        name=free_memory\
+        name="free_memory"\
         full_text="$(free -w | awk 'BEGIN{i=0;s=""}{i=i+1;if (i>1) s=s$4"/"$2"*100,"}END{print s}' |sed -e 's/,/\n/g'|bc -l |sed -e 's/\([0-9]\+\)\..*/\1%/' |awk 'BEGIN{i=0}{if (i==0) printf "Mem: R:"$0"|C:"; else printf $0; i=i+1}')"\
         color="#d9922e"
+    separator
+    module\
+        name="SSD Space"\
+        full_text="$(df -h | awk '/mapper/{print $6 ": " $5 "|"}' | tr -d '\n' | sed 's/|$//')"\
+        color="#d92e92"
     separator
     module\
         name="wifi"\
@@ -27,7 +32,7 @@ while :; do
     separator
     module\
         name="sound"\
-        full_text="Vol: $(amixer sget 'Master' | grep -e '%' | grep Left | sed 's/[^[]*\[\([0-9]\+%\)\].*/\1/g')"\
+        full_text="Vol: $(read -r volume status<<<$(amixer sget Master|grep Left | awk -F"[][]" '/%/{print gensub(/%/,"","g",$2) "\t" $4}');echo $volume$(case $status in on) printf "%%";; off) printf "M";; esac))"\
         color="#d92e92"
     separator
     module\
@@ -36,7 +41,7 @@ while :; do
         color="#92d92e"
     separator
     module\
-        name=battery\
+        name="battery"\
         full_text="Bat: $(acpi | sed 's/.* \([0-9]\+%\).*/\1/g')"\
         color=$(case $(acpi|tr -d ','|awk '{print $3}') in 'Charging') echo '#20e0a0' ;; 'Discharging') echo '#ff9020' ;; 'Unknown') echo '#d9d92e' ;; esac)
     echo ']'
